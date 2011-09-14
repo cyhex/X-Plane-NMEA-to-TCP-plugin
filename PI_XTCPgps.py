b0586f092f61f96f3dffc9704ade1a8dba3e7dbc
Timor A <timor@cyhex.com>
2011-09-14 19:59:33 +0200

    ident fixed, checked in x-plane 9 -works with xsoar...x

--- PI_XTCPgps.py
+++ PI_XTCPgps.py
@@ -98,7 +98,7 @@ class PythonInterface:
             self.CannotWrite = after_count - before_count
     
             self.OutputFile.write(str(before_count) + '  ' + str(after_count) + '  ' + str(self.CannotWrite) + '\n')
-                self.LineCount = self.LineCount + 1
+            self.LineCount = self.LineCount + 1
     
             # Locate data references for all communicated variables.
     
@@ -128,123 +128,124 @@ class PythonInterface:
             return self.Name, self.Sig, self.Desc
 
         def XPluginStop(self):
-        # Unregister the callback.
-        XPLMUnregisterFlightLoopCallback(self, self.FlightLoopCB, 0)
-        self.OutputFile.close()
+            # Unregister the callback.
+            XPLMUnregisterFlightLoopCallback(self, self.FlightLoopCB, 0)
+            self.OutputFile.close()
 
         def XPluginEnable(self):
-        return 1
+            return 1
 
         def XPluginDisable(self):
-        pass
+            pass
 
         def XPluginReceiveMessage(self, inFromWho, inMessage, inParam):
-        pass
+            pass
 
         def FlightLoopCallback(self, elapsedMe, elapsedSim, counter, refcon):
-
-        if self.CannotWrite:
-            return 10.0
-
-#        self.where = 1
-#        self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
-#            self.LineCount = self.LineCount + 1
-
-        # Get current values for communicated variables.
-        self.Zulu_time = XPLMGetDataf(self.drZulu_time)  # sec since midnight
-        self.Lat_deg = XPLMGetDatad(self.drLat_deg)
-        self.Lon_deg = XPLMGetDatad(self.drLon_deg)
-        self.Vgnd_kts = XPLMGetDataf(self.drVgnd_kts)*1.943  # m/sec -> kts
-        self.Hding_mag = XPLMGetDataf(self.drHding_mag)
-        self.Mag_var = XPLMGetDataf(self.drMag_var)
-        self.Alt_ind = XPLMGetDatad(self.drAlt_ind)
-
-        # put in nmea format (matches x-plane | equipment | nmea serial feed)
-        # time ssss.ssss since midnight --> hhmmss
-        hh  = int(self.Zulu_time/3600)
-        mm = int(self.Zulu_time/60) - 60*hh
-        ss = int(round(self.Zulu_time,0)) - 3600*hh - 60*mm
-        n_time = str(hh).zfill(2)    \
-               + str(mm).zfill(2)    \
-               + str(ss).zfill(2) + '.00'
-
-        # lat and lon +/- ddd.dddd --> dddmm.mmm,h
-        h = 'N'
-        if self.Lat_deg < 0:
-            h = 'S'
-        self.Lat_deg = abs(self.Lat_deg)
-        ddd = int(self.Lat_deg)
-        mmm = 60*(self.Lat_deg - ddd)
-        n_lat = str(ddd).zfill(2)        \
-                      + ('%.4f' % mmm).zfill(7) + ','    \
-                      + h
-
-        h = 'E'
-        if self.Lon_deg < 0:
-            h = 'W'
-        self.Lon_deg = abs(self.Lon_deg)
-        ddd = int(self.Lon_deg)
-        mmm = 60*(self.Lon_deg - ddd)
-        n_lon = str(ddd).zfill(3)        \
-                      + ('%.4f' % mmm).zfill(7) + ','    \
-                      + h
-
-        # speed and heading may need some padding
-        n_speed = ('%.1f' % self.Vgnd_kts).zfill(5)
-        # heading mag --> true
-        n_heading = '%.1f' % (self.Hding_mag - self.Mag_var)
-
-        # date set once above 
-
-        # magnetic variation +/- dd.dddd --> ddd.d,h
-        h = 'W'
-        if self.Mag_var < 0:
+    
+            if self.CannotWrite:
+    
+                return 10.0
+    
+    #        self.where = 1
+    #        self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
+    #            self.LineCount = self.LineCount + 1
+    
+            # Get current values for communicated variables.
+            self.Zulu_time = XPLMGetDataf(self.drZulu_time)  # sec since midnight
+            self.Lat_deg = XPLMGetDatad(self.drLat_deg)
+            self.Lon_deg = XPLMGetDatad(self.drLon_deg)
+            self.Vgnd_kts = XPLMGetDataf(self.drVgnd_kts)*1.943  # m/sec -> kts
+            self.Hding_mag = XPLMGetDataf(self.drHding_mag)
+            self.Mag_var = XPLMGetDataf(self.drMag_var)
+            self.Alt_ind = XPLMGetDatad(self.drAlt_ind)
+    
+            # put in nmea format (matches x-plane | equipment | nmea serial feed)
+            # time ssss.ssss since midnight --> hhmmss
+            hh  = int(self.Zulu_time/3600)
+            mm = int(self.Zulu_time/60) - 60*hh
+            ss = int(round(self.Zulu_time,0)) - 3600*hh - 60*mm
+            n_time = str(hh).zfill(2)    \
+                   + str(mm).zfill(2)    \
+                   + str(ss).zfill(2) + '.00'
+    
+            # lat and lon +/- ddd.dddd --> dddmm.mmm,h
+            h = 'N'
+            if self.Lat_deg < 0:
+                h = 'S'
+            self.Lat_deg = abs(self.Lat_deg)
+            ddd = int(self.Lat_deg)
+            mmm = 60*(self.Lat_deg - ddd)
+            n_lat = str(ddd).zfill(2)        \
+                          + ('%.4f' % mmm).zfill(7) + ','    \
+                          + h
+    
             h = 'E'
-        self.Mag_var = abs(self.Mag_var)
-        ddd = '%.1f' % self.Mag_var
-        n_magvar = str(ddd)    + ','    \
-                 + h
-
-        # altitude meters
-        n_alt = '%.1f' % self.Alt_ind
-
-        # construct the nmea gprmc sentence
-        gprmc = 'GPRMC'        + ','    \
-              + n_time        + ','    \
-              + 'A'        + ','    \
-              + n_lat        + ','    \
-              + n_lon        + ','    \
-              + n_speed        + ','    \
-              + n_heading    + ','    \
-              + self.n_date    + ','    \
-              + n_magvar
-
-        # append check sum and inital $
-        cks = cksum(gprmc)
-        gprmc = '$' + gprmc + '*' + cks + '\r\n'
-
-        # construct the nmea gpgga sentence
-        gpgga = 'GPGGA'        + ','    \
-              + n_time        + ','    \
-              + n_lat        + ','    \
-              + n_lon        + ',1,04,0.0,'    \
-              + n_alt        + ',M,,,,'
-
-        # append check sum and inital $
-        cks = cksum(gpgga)
-        gpgga = '$' + gpgga + '*' + cks + '\r\n'
-
-        # pocketfms requires gpgsa sentence;
-        # this one (constant) is what x-plane equipment setting sends.
-        gpgsa = "$GPGSA,A,3,13,20,31,,,,,,,,,,02.2,02.2,*1e\r\n"
-
-#        serial write at 4800 baud can take .3 sec, so put in own thread;
-        write_thread = threading.Thread(target=self.ser.write, args=(gprmc + gpgga + gpgsa,))
-        write_thread.start()
-
-#        self.where = 5
-#        self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
-#            self.LineCount = self.LineCount + 1
-
-        # Return s.s to indicate that we want to be called again in s.s seconds.
-        return 1.0
+            if self.Lon_deg < 0:
+                h = 'W'
+            self.Lon_deg = abs(self.Lon_deg)
+            ddd = int(self.Lon_deg)
+            mmm = 60*(self.Lon_deg - ddd)
+            n_lon = str(ddd).zfill(3)        \
+                          + ('%.4f' % mmm).zfill(7) + ','    \
+                          + h
+    
+            # speed and heading may need some padding
+            n_speed = ('%.1f' % self.Vgnd_kts).zfill(5)
+            # heading mag --> true
+            n_heading = '%.1f' % (self.Hding_mag - self.Mag_var)
+    
+            # date set once above 
+    
+            # magnetic variation +/- dd.dddd --> ddd.d,h
+            h = 'W'
+            if self.Mag_var < 0:
+                h = 'E'
+            self.Mag_var = abs(self.Mag_var)
+            ddd = '%.1f' % self.Mag_var
+            n_magvar = str(ddd)    + ','    \
+                     + h
+    
+            # altitude meters
+            n_alt = '%.1f' % self.Alt_ind
+    
+            # construct the nmea gprmc sentence
+            gprmc = 'GPRMC'        + ','    \
+                  + n_time        + ','    \
+                  + 'A'        + ','    \
+                  + n_lat        + ','    \
+                  + n_lon        + ','    \
+                  + n_speed        + ','    \
+                  + n_heading    + ','    \
+                  + self.n_date    + ','    \
+                  + n_magvar
+    
+            # append check sum and inital $
+            cks = cksum(gprmc)
+            gprmc = '$' + gprmc + '*' + cks + '\r\n'
+    
+            # construct the nmea gpgga sentence
+            gpgga = 'GPGGA'        + ','    \
+                  + n_time        + ','    \
+                  + n_lat        + ','    \
+                  + n_lon        + ',1,04,0.0,'    \
+                  + n_alt        + ',M,,,,'
+    
+            # append check sum and inital $
+            cks = cksum(gpgga)
+            gpgga = '$' + gpgga + '*' + cks + '\r\n'
+    
+            # pocketfms requires gpgsa sentence;
+            # this one (constant) is what x-plane equipment setting sends.
+            gpgsa = "$GPGSA,A,3,13,20,31,,,,,,,,,,02.2,02.2,*1e\r\n"
+    
+    #        serial write at 4800 baud can take .3 sec, so put in own thread;
+            write_thread = threading.Thread(target=self.ser.write, args=(gprmc + gpgga + gpgsa,))
+            write_thread.start()
+    
+    #        self.where = 5
+    #        self.OutputFile.write('where?  ' + str(self.where) + '  ' + str(time.clock()) + '\n')
+    #            self.LineCount = self.LineCount + 1
+    
+            # Return s.s to indicate that we want to be called again in s.s seconds.
+            return 1.0
